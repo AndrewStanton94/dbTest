c92a48420776fc8ce72bb05485f713126813f677
Alpha1594 <alpha1594@gmail.com>
2015-04-01 01:00:53 +0100

    Tabs to spaces

--- api/index.php
+++ api/index.php
@@ -1,27 +1,27 @@
 <?php
 switch ($_SERVER['REQUEST_METHOD']) {
-    case "POST":					// Creation
-		$retValue = "prodId " . $_POST["prodId"] . "\n " .
-			" prodParent " . $_POST["prodParent"] . "\n " .
-			"prodCat " . $_POST["prodCat"] . "\n " .
-			"prodURL <a href=' " . $_POST["prodURL"] . "'>" .$_POST["prodURL"] .  "</a> \n " .
-			"prodCap " . $_POST["prodCap"] ;
-		echo nl2br ($retValue);		// \n to <br>
-		break;
+    case "POST":                    // Creation
+        $retValue = "prodId " . $_POST["prodId"] . "\n " .
+            " prodParent " . $_POST["prodParent"] . "\n " .
+            "prodCat " . $_POST["prodCat"] . "\n " .
+            "prodURL <a href=' " . $_POST["prodURL"] . "'>" .$_POST["prodURL"] .  "</a> \n " .
+            "prodCap " . $_POST["prodCap"] ;
+        echo nl2br ($retValue);     // \n to <br>
+        break;
 
     case "GET":
-    	echo "Get";
-		break;
+        echo "Get";
+        break;
 
-	case "PUT":						// Update
-	    	echo "Put";
-			break;
+    case "PUT":                     // Update
+            echo "Put";
+            break;
 
-	case "DELETE":
-	    	echo "Delete";
-			break;
+    case "DELETE":
+            echo "Delete";
+            break;
 
-	default:
-        echo "Error";	
+    default:
+        echo "Error";   
 }
 ?>
