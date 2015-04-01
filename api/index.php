18942379051d4bb8b95df1e26eb181f0353bceb1
Alpha1594 <alpha1594@gmail.com>
2015-04-01 01:07:14 +0100

    Removed errant space

--- api/index.php
+++ api/index.php
@@ -2,7 +2,7 @@
 switch ($_SERVER['REQUEST_METHOD']) {
     case "POST":                    // Creation
         $retValue = "prodId " . $_POST["prodId"] . "\n " .
-            " prodParent " . $_POST["prodParent"] . "\n " .
+            "prodParent " . $_POST["prodParent"] . "\n " .
             "prodCat " . $_POST["prodCat"] . "\n " .
             "prodURL <a href=' " . $_POST["prodURL"] . "'>" .$_POST["prodURL"] .  "</a> \n " .
             "prodCap " . $_POST["prodCap"] ;
