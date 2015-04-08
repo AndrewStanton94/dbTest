44a2a12e88609bdccc184a8605a04453961743c6
AndrewStanton94 <alpha1594@gmail.com>
2015-04-08 15:12:56 +0100

    Don't copy git data to server

--- toHtdocs.bat
+++ toHtdocs.bat
@@ -1,1 +1,1 @@
-xcopy "C:\Users\A.2\Documents\My Dropbox\_Port\WEBSCRP\dbTest" F:\xampp\htdocs\dbV2 /e /y
+xcopy "C:\Users\A.2\Documents\My Dropbox\_Port\WEBSCRP\dbTest" F:\xampp\htdocs\dbV2 /e /y /EXCLUDE:noCopy.txt
