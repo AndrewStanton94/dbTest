5b9eb92bc25642a0b1a9f63a04a8674783bb4d7d
Andrew Stanton <up683693@myport.ac.uk>
2015-04-10 02:12:37 +0100

    Back on laptop, modified filepath

--- toHtdocs.bat
+++ toHtdocs.bat
@@ -1,1 +1,1 @@
-xcopy "C:\Users\A.2\Documents\My Dropbox\_Port\WEBSCRP\dbTest" F:\xampp\htdocs\dbV2 /e /y /EXCLUDE:noCopy.txt
+xcopy "C:\Users\Andrew\Dropbox\_Port\WEBSCRP\dbTest" F:\xampp\htdocs\dbV2 /e /y /EXCLUDE:noCopy.txt
