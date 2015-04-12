9cd2887cbf5832ef2c09341e4d19883b8e391fdb
Andrew Stanton <up683693@myport.ac.uk>
2015-04-12 23:36:17 +0100

    Refactored code, removed unneeded variables

--- fileUploadSS.php
+++ fileUploadSS.php
@@ -1,26 +1,23 @@
 <?php
 if(isset($_FILES['image'])){
     $errors= array();
-    $file_name = $_FILES['image']['name'];
-    $file_size =$_FILES['image']['size'];
-    $file_tmp =$_FILES['image']['tmp_name'];
-    $file_type=$_FILES['image']['type'];   
-    $file_ext=strtolower(end(explode('.', $file_name)));
-    $extensions = array("jpeg","jpg","png");        
-    if(in_array($file_ext,$extensions )=== false){
-        $errors[]="extension not allowed, please choose a JPEG or PNG file.";
-    }
+    $file_name = $_FILES['image']['name'];      // Original name
+    $file_tmp = $_FILES['image']['tmp_name'];   // Server allocated name
+    $file_ext = strtolower(end(explode('.', $file_name)));
 
-    if($file_size > 2097152){
-        $errors[]='File size must be at most 2 MB';
-    }
-
-    if(empty($errors)==true){
-        move_uploaded_file($file_tmp,"images/".$file_name);
-        echo ["Success"];
+    if(in_array($file_ext, array('jpeg','jpg','png'))){
+        if($_FILES['image']['size'] < 2097152){
+            move_uploaded_file($file_tmp, "images/".$file_name);
+            $errors[]='Success';
+        }
+        else{
+            $errors[]='File size must be at most 2 MB';
+        }
     }
     else{
-        print_r($errors);
+        $errors[]='extension not allowed, please choose a JPEG or PNG file.';
     }
+
+    echo json_encode($errors);
 }
 ?>
