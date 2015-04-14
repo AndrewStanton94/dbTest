273c29c3d2884b7edba44170d9734ec031e03705
Andrew Stanton <up683693@myport.ac.uk>
2015-04-14 17:43:39 +0100

    Moved to different device

--- toHtdocs.bat
+++ toHtdocs.bat
@@ -1,1 +1,1 @@
-xcopy "C:\Users\Andrew\Dropbox\_Port\WEBSCRP\dbTest" F:\xampp\htdocs\dbV2 /e /y /EXCLUDE:noCopy.txt
+xcopy "C:\Users\Andrew\Dropbox\_Port\WEBSCRP\dbTest" D:\xampp\htdocs\dbV2 /e /y /EXCLUDE:noCopy.txt
