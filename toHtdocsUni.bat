53bd4ad25e61326cbb06538091c26692409d0235
AndrewStanton94 <up683693@myport.ac.uk>
2015-04-17 15:56:41 +0100

    Before :s/innterText/textContent

--- toHtdocsUni.bat
+++ toHtdocsUni.bat
@@ -1,1 +1,1 @@
-xcopy "N:\WEBSCRP\dbTest" E:\xampp\htdocs\dbV2 /e /y /EXCLUDE:N:\WEBSCRP\dbTest\noCopy.txt
+xcopy "N:\WEBSCRP\dbTest" D:\xampp\htdocs\dbV2 /e /y /EXCLUDE:N:\WEBSCRP\dbTest\noCopy.txt
